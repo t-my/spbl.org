f566891071710b044aa829c1727cad74ccbdf3e8
Tuomas-Matti Soikkeli <tsoikkeli@gmail.com>
2016-06-21 23:22:38 +0300

    Fix footers

--- src/lajiesittely/index.php
+++ src/lajiesittely/index.php
@@ -107,20 +107,20 @@
             <div class="row">
               <div class="col-md-12">
                 <a href="/"><img id="logo" alt="spbl.org logo" src="/images/logo-web.jpg"/></a>
-                <hr/>
                 <ul class="simple-list">
                   <li class="active"><a href="/">Etusivu</a></li>
-                  <li><a href="sample.html">Lajista</a></li>
-                  <li><a href="#spbl">Suomen Paintball-liitto</a></li>
-                  <li><a href="#news">Tiedotteet</a></li>
-                  <li><a href="#events">Tapahtumat</a></li>
-                  <li><a href="#contact">Yhteys</a></li>
+                  <li><a href="/lajiesittely">Lajista</a></li>
+                  <li><a href="/tiedotteet">Tiedotteet</a></li>
+                  <li><a href="/julkaisut">Julkaisut</a></li>
+                  <li><a href="/tapahtumat">Tapahtumat</a></li>
+                  <li><a href="/joukkueet">Joukkueet</a></li>
+                  <li><a href="/yhteys">Yhteys</a></li>
                 </ul>
               </div>
             </div>
             <div class="row">
-              <div class="col-md-12 right">
-                spbl.org &copy; 2015
+              <div class="col-md-2 right">
+                spbl.org &copy; 2016
               </div>
             </div>
           </div>
